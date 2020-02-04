c9ff544ae3da6cb080c8f8e1aa8638e4fa5740fc
Aristoteles <aristotelesbr2014@gmail.com>
2020-02-03 21:42:13 -0300

    Added Product Controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers {
+    [Route ("products")]
+    public class ProductController : ControllerBase {
+        [HttpGet]
+        [Route ("")]
+        public async Task<ActionResult<List<Product>>> List ([FromServices] DataContext context) {
+            var products = await context
+                .Products
+                .Include (x => x.Category)
+                .AsNoTracking ()
+                .ToListAsync ();
+
+            return Ok (products);
+        }
+
+        [HttpGet]
+        [Route ("categories/{id:int}")]
+        public async Task<ActionResult<Product>> Show (int id, [FromServices] DataContext context) {
+            var category = await context
+                .Products
+                .Include (x => x.Category)
+                .AsNoTracking ()
+                .Where (x => x.CategoryId == id)
+                .ToListAsync ();
+
+            return Ok (category);
+        }
+
+        [HttpPost]
+        [Route ("")]
+        public async Task<ActionResult<Product>> Create (
+            [FromBody] Product model, [FromServices] DataContext context) {
+
+            if (!ModelState.IsValid) {
+                return BadRequest (ModelState);
+            }
+
+            try {
+                context.Products.Add (model);
+                await context.SaveChangesAsync ();
+                return Ok (model);
+            } catch {
+                return BadRequest (new { message = "Houve um erro. Por favor tente novamente." });
+            }
+        }
+
+    }
+}
